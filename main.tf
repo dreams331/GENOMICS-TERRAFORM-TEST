fa3eb187462ab9704b4e8d9011ee1067c1d2bc3f
dreams331 <100285594+dreams331@users.noreply.github.com>
2022-06-21 17:13:09 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -97,19 +97,7 @@ resource "aws_s3_bucket" "incoming_bucket" {
   bucket = "your-bucket-name"
 }
 
-#CREATING S3 BUCKET NOTIFICATION TO SEND TO LAMBDA
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = aws_s3_bucket.incoming_bucket.id
 
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.test_lambda.arn
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "AWSLogs/"
-    filter_suffix       = ".log"
-  }
-
-  depends_on = [aws_lambda_permission.allow_bucket]
-}
 
  
 # CREATING OUTPUT S3 BUCKET
