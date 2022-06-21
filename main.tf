fef0ea74d99a3fd8d5c6a06b44a84e8a5b2834e3
dreams331 <100285594+dreams331@users.noreply.github.com>
2022-06-21 12:00:25 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -69,11 +69,14 @@ resource "aws_lambda_function" "test_lambda" {
   function_name = "lambda"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda.lambda_handler"
-
+  role          = aws_iam_role.iam_for_lambda.arn
+  runtime = "python3.9"
   source_code_hash = data.archive_file.lambda.output_base64sha256
-
-  runtime = "python3.8"
-  
+  environment {
+    variables = {
+      incoming_bucket_arn = aws_s3_bucket.incoming_bucket.arn
+    }
+  }
 }
 
 
@@ -88,11 +91,12 @@ resource "aws_lambda_permission" "allow_bucket" {
 }
 
 
-
+# CREATING S3 BUCKET
 resource "aws_s3_bucket" "incoming_bucket" {
   bucket = "your-bucket-name"
 }
 
+#CREATING S3 BUCKET NOTIFICATION TO SEND TO LAMBDA
 resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = aws_s3_bucket.incoming_bucket.id
 
