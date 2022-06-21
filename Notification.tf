0f4696e8bdc220eef4ecf970ebf2b2b1b0750eae
dreams331 <100285594+dreams331@users.noreply.github.com>
2022-06-21 17:13:31 +0100

    Update Notification.tf

--- Notification.tf
+++ Notification.tf
@@ -1,1 +1,13 @@
+#CREATING S3 BUCKET NOTIFICATION TO SEND TO LAMBDA
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.incoming_bucket.id
 
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.test_lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "AWSLogs/"
+    filter_suffix       = ".log"
+  }
+
+  depends_on = [aws_lambda_permission.allow_bucket]
+}
